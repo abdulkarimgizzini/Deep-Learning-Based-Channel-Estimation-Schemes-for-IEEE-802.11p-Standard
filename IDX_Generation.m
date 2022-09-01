cd5b26d06c7e8840020e3e4cf7c92f8613ed6b38
Abdul Karim Gizzini <78584545+abdulkarimgizzini@users.noreply.github.com>
2022-09-01 17:57:44 +0200

    Training/Testing Indices Splitting

--- IDX_Generation.m
+++ IDX_Generation.m
@@ -5,4 +5,4 @@ TE  = 0.20; % testing dataset percentage
 All_IDX = 1:IDX;
 training_samples = randperm(IDX,TR*IDX).'; % choose randomly 80% of the total indices for training
 testing_samples = setdiff(All_IDX,training_samples).';
-save(['./samples_indices_',num2str(IDX),'.mat'],  'training_samples','testing_samples');
+save(['./samples_indices_',num2str(IDX),'.mat'],  'training_samples','testing_samples'); 
